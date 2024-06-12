cf06766d890deb92d0d17b4640b345876af5eb04
Sven <doingscience@posteo.de>
2024-06-12 14:39:04 +0200

    first work on threading

--- src/task.py
+++ src/task.py
@@ -1,3 +1,4 @@
+import sorting
 import time
 class task:
     def __init__(self, dataset):
@@ -17,7 +18,57 @@ class bubblesort(task):
 
     def sort(self):
         self._start()
-        #do-something
+        sorting.bubble(self.dataset)
         self._end()
         self._taketime()
         pass
+
+class insertion_sort(task):
+    def sort(self):
+        self._start()
+        self._insertion_sort(self.dataset)
+        self._end()
+        self._taketime()
+        pass
+
+    def _insertion_sort(self, arr):
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and key < arr[j]:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
+        return arr
+
+class merge_sort(task):
+    def sort(self):
+        self._start()
+        sorting.merge(self.dataset)
+        self._end()
+        self._taketime()
+        pass
+
+
+class quick_sort(task):
+    def sort(self):
+        self._start()
+        sorting.quick(self.dataset)
+        self._end()
+        self._taketime()
+        pass
+
+class heap_sort(task):
+    def sort(self):
+        self._start()
+        # do-something
+        self._end()
+        self._taketime()
+        pass
+
+
+def main():
+    print("what kind of scheduler do you want to use?")
+    print("Option 1:    First Come First Serve")
+    print("Option 2:    Shortest Job First")
+    print("Option 3:    ")
--- src/task_scheduler.py
+++ src/task_scheduler.py
@@ -1,9 +1,30 @@
+import queue
+
 import apscheduler as sch
 import sorting
 import numpy as np
 import pandas as pd
+import threading as thread
 
+thread1 =   0
+thread2 =   0
 
+queue_thread1 = queue.Queue()
+queue_thread2 = queue.Queue()
+def assign_thread1(task):
+    queue_thread1.put(task)
+
+def assign_thread2(task):
+    queue_thread2.put(task)
+
+def create_queue():
+    '''
+    Wir müssen eine Queue an Tasks erstellen, deren Elemente an die Threads übergeben werden.
+    Wird ein Element übergeben, wird es aus der Queue entfernt.
+    Gibt es keine Elemente in der Queue und die Threads haben ihre Aufgaben erfüllt,
+    dann soll das Hauptprogrmam beendet werden
+    :return:
+    '''
 def create_dataset(start=1, end=101, size=200):
     print("generating "+str(size)+" numbers between "+str(start)+" and "+str(size))
 
@@ -11,7 +32,32 @@ def create_dataset(start=1, end=101, size=200):
     df = pd.DataFrame(array)
     df.to_csv("dataset.csv", index=False)
 
+
 def dataset_to_array(dataset):
     dataframe = pd.read_csv(dataset)
     return dataframe.to_numpy()
+
+def execute(tasks):
+    '''
+    This is the function which keeps the thread going.
+    It executes all things in the queue.
+    Is it filled with tasks, it executes them
+    Is it filled with a None-Element, then it shuts down
+
+    :param tasks: A queue full of instances of the class task
+    :return:
+    '''
+    working = 0
+    while True:
+        try:
+            task = tasks.get_nowait()
+        except queue.Empty:
+            continue
+
+        if task is None:
+            print("Queue is None - ending Thread!")
+            break
+        working = 1
+        task.sort()
+        working = 0
 
